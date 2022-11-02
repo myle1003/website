425ac05ead3869ca2900cd7d5b5c2b8cee5ea6e2
MyTLTT <myle10032001@gmail.com>
2022-11-02 16:19:35 +0700

    11/2/2022

---
+++ index.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+
+
+
+        .all-browsers > h1, .browser {
+            margin: 10px;
+            padding: 5px;
+        }
+
+
+        .browser > h2, p {
+            margin: 4px;
+            font-size: 90%;
+        }
+        img {
+            position : absolute;
+            top : 100px;
+            left: 200px;
+
+        }
+
+        footer {
+            position : absolute;
+            bottom: 0;
+            width: 100%;
+            text-align: center;
+            padding: 5px;
+            background-color: lightgray;
+            color: black;
+        }
+        * {
+            box-sizing: border-box;
+        }
+
+        body {
+            margin: 0;
+        }
+
+        /* Style the header */
+
+
+        /* Style the top navigation bar */
+        .topnav {
+            overflow: hidden;
+            background-color: #333;
+        }
+
+        /* Style the topnav links */
+        .topnav a {
+            float: left;
+            display: block;
+            color: #f2f2f2;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        /* Change color on hover */
+        .topnav a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            width: 200px;
+            background-color: #f1f1f1;
+        }
+
+        li a {
+            display: block;
+            color: #000;
+            padding: 8px 16px;
+            text-decoration: none;
+        }
+
+        /* Change the link color on hover */
+        li a:hover {
+            background-color: #555;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+    <nav>
+        <div class="topnav">
+            <a href="#">WELCOME WEBSITE</a>
+            <a href="#">SIGN IN</a>
+            <a href="#">SIGN UP</a>
+        </div>
+    </nav>
+    <div>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">About</a></li>
+            <img src="./images/beaty.png" alt="">
+
+        </ul>
+    </div>
+
+    <footer>
+        <p>Contact with me!!!!!</p>
+    </footer>
+
+
+</body>
+</html>
